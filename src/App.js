237a9ac5e17004a13e08d90191e9a0626feaa629
JakubTomaszewski <jakub.k.tomaszewski@gmail.com>
2022-04-23 12:00:44 +0200

    PZSP2-36 Feat: Display questions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Questions from "./components/Questions";
+import Question from "./components/Question";
+
+const questions = [
+  {
+    id: 1,
+    text: "Gdzie urodził się Kopernik?",
+    answers: ["w Warszawie", "w Grańsku", "w Berlinie", "w Toruniu"],
+  },
+  {
+    id: 2,
+    text: "Gdzie mieszka Papież?",
+    answers: ["w Warszawie", "w Watykanie", "w Berlinie", "w Toruniu"],
+  },
+  {
+    id: 3,
+    text: "Ile voltów mamy w gniazdku?",
+    answers: ["230", "50", "1000", "2"],
+  },
+];
 
 function App() {
   return (
-    <div>
+    <div className="questions">
       <h2>Questions</h2>
+      <Questions questions={questions} />
+      {/* <Question />
+      <Question />
+      <Question /> */}
     </div>
   );
 }
