f917ede10306ad6564103a9c10c287e18b9becb0
Jakub Tomaszewski <Kuba@MacBook-Pro.local>
2022-05-18 14:11:59 +0200

    My local repo has crashed

---
+++ src/components/NewAnswer.js
@@ -0,0 +1,79 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Checkbox from "@mui/material/Checkbox";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
+
+const NewAnswer = ({
+  index,
+  content,
+  isCorrect,
+  handleRemove,
+  handleCheck,
+  handleInput,
+}) => {
+  const [checked, setChecked] = useState(isCorrect);
+  const [text, setText] = useState(content);
+
+  function handleCheckbox(event) {
+    setChecked(event.target.checked);
+  }
+
+  function handleInputEvent(event) {
+    setText(event.target.value);
+    handleInput(event.target.value, index);
+  }
+
+  return (
+    <Grid
+      container
+      spacing={1.5}
+      style={{
+        width: "100%",
+        margin: "auto",
+        padding: "0px",
+        alignItems: "center",
+      }}
+    >
+      <Grid item xs={9}>
+        <TextField
+          required
+          multiline
+          id="answer-content"
+          label="Odpowiedź"
+          variant="outlined"
+          value={text}
+          onChange={(e) => handleInputEvent(e)}
+          style={{
+            textAlign: "center",
+            padding: "0px 0px",
+            margin: "auto",
+            marginLeft: "0%",
+            width: "100%",
+          }}
+        />
+      </Grid>
+      <Grid item xs={1}>
+        <Checkbox
+          checked={checked}
+          onChange={(event) => {
+            handleCheck(index);
+            handleCheckbox(event);
+          }}
+          color="success"
+          size="medium"
+          label="Poprawna"
+        />
+      </Grid>
+      <Grid item xs={1}>
+        <IconButton aria-label="delete" size="large">
+          <DeleteIcon onClick={() => handleRemove(index)} />
+        </IconButton>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default NewAnswer;
