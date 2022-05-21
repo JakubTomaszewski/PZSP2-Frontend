b57b0fbe4284e8e1d42cb7ec59d0a164dc28fbb9
Jakub Tomaszewski <Kuba@MacBook-Pro.local>
2022-05-21 20:43:14 +0200

    PZSP2-47 Feat: Add adding open type question functionality

--- src/components/AddQuestion.js
+++ src/components/AddQuestion.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { useState } from "react";
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import SendIcon from "@mui/icons-material/Send";
 import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
 import NewAnswer from "./NewAnswer";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
 
 const AddQuestion = ({ addQuestion }) => {
+  const [openTypeQuestion, setOpenTypeQuestion] = useState(false);
   const [questionContent, setText] = useState("");
   const [answersList, setAnswersList] = useState([
     { content: "", isCorrect: false },
@@ -21,35 +25,49 @@ const AddQuestion = ({ addQuestion }) => {
       return;
     }
 
-    const question = {
+    // open type question
+    let question = {
+      type: "o",
       content: questionContent,
-      answers: answersList.map((question) => question.content),
-      areCorrect: answersList.map((question) => question.isCorrect),
-      closed: true,
     };
 
-    // At least 2 answers
-    if (question.closed && question.answers.length < 2) {
-      alert("Proszę dodać więcej odpowiedzi");
-      return;
-    }
+    if (!openTypeQuestion) {
+      // closed type question
+      question = {
+        type: "c",
+        content: questionContent,
+        answers: answersList.map((question) => question.content),
+        areCorrect: answersList.map((question) => question.isCorrect),
+      };
 
-    // At least one correct answer
-    if (!question.areCorrect.some((ans) => ans === true)) {
-      alert("Przynajmniej jedna odpowiedź musi być poprawna");
-      return;
-    }
+      // TODO: create function checkNewQuestionCorrectness
+      // At least 2 answers
+      if (question.answers.length < 2) {
+        alert("Proszę dodać więcej odpowiedzi");
+        return;
+      }
 
-    // No blank answers
-    if (question.answers.some((ans) => ans === "")) {
-      alert("Odpowiedzi nie mogą być puste");
-      return;
+      // At least one correct answer
+      if (!question.areCorrect.some((ans) => ans === true)) {
+        alert("Przynajmniej jedna odpowiedź musi być poprawna");
+        return;
+      }
+
+      // No blank answers
+      if (question.answers.some((ans) => ans === "")) {
+        alert("Odpowiedzi nie mogą być puste");
+        return;
+      }
     }
 
     addQuestion(question);
 
-    setText("");
     setAnswersList([{ content: "", isCorrect: false }]);
+    setText("");
+  }
+
+  function handleSwitch(event) {
+    setOpenTypeQuestion(event.target.checked);
   }
 
   function handleCheck(index) {
@@ -84,66 +102,127 @@ const AddQuestion = ({ addQuestion }) => {
       noValidate
       autoComplete="off"
     >
-      <Grid
-        container
-        spacing={1.5}
+      <FormControlLabel
         style={{
-          width: "90%",
-          margin: "auto",
-          padding: "0px",
           alignItems: "center",
+          marginLeft: "5%",
         }}
-      >
-        <Grid item xs={12}>
-          <TextField
-            required
-            multiline
-            id="question-content"
-            label="Treść pytania"
-            variant="outlined"
-            value={questionContent}
-            onChange={(e) => setText(e.target.value)}
-            style={{
-              textAlign: "center",
-              padding: "0px 0px",
-              margin: "auto",
-              marginLeft: "0%",
-              width: "100%",
-            }}
+        control={
+          <Switch
+            checked={openTypeQuestion}
+            onChange={handleSwitch}
+            inputProps={{ "aria-label": "controlled" }}
           />
+        }
+        label="Pytanie otwarte"
+      />
+
+      {openTypeQuestion ? (
+        <Grid
+          container
+          spacing={1.5}
+          style={{
+            width: "90%",
+            margin: "auto",
+            padding: "0px",
+            alignItems: "center",
+          }}
+        >
+          <Grid item xs={12}>
+            <TextField
+              required
+              multiline
+              id="question-content"
+              label="Treść pytania"
+              variant="outlined"
+              value={questionContent}
+              onChange={(e) => setText(e.target.value)}
+              rows={3}
+              style={{
+                textAlign: "center",
+                padding: "0px 0px",
+                margin: "auto",
+                marginLeft: "0%",
+                width: "100%",
+              }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              variant="contained"
+              endIcon={<SendIcon />}
+              type="submit"
+              style={{ width: "100%" }}
+            >
+              Zapisz
+            </Button>
+          </Grid>
         </Grid>
-        {answersList.map((answer, i) => (
-          <NewAnswer
-            index={i}
-            key={i}
-            content={answer.content}
-            isCorrect={answer.isCorrect}
-            handleCheck={handleCheck}
-            handleRemove={handleRemove}
-            handleInput={handleInput}
-          />
-        ))}
-        <Grid item xs={6}>
-          <Button
-            variant="contained"
-            endIcon={<AddCircleOutlineIcon />}
-            onClick={addNewAnswer}
-            style={{ width: "100%" }}
-          >
-            Dodaj odpowiedź
-          </Button>
+      ) : (
+        <Grid
+          container
+          spacing={1.5}
+          style={{
+            width: "90%",
+            margin: "auto",
+            padding: "0px",
+            alignItems: "center",
+          }}
+        >
+          <Grid item xs={12}>
+            <TextField
+              required
+              multiline
+              id="question-content"
+              label="Treść pytania"
+              variant="outlined"
+              value={questionContent}
+              onChange={(e) => setText(e.target.value)}
+              rows={3}
+              style={{
+                textAlign: "center",
+                padding: "0px 0px",
+                margin: "auto",
+                marginLeft: "0%",
+                width: "100%",
+              }}
+            />
+          </Grid>
+          {answersList.map((answer, i) => (
+            <NewAnswer
+              index={i}
+              key={i}
+              content={answer.content}
+              isCorrect={answer.isCorrect}
+              handleCheck={handleCheck}
+              handleRemove={handleRemove}
+              handleInput={handleInput}
+            />
+          ))}
+          <Grid item xs={6}>
+            <Typography noWrap>
+              <Button
+                variant="contained"
+                endIcon={<AddCircleOutlineIcon />}
+                onClick={addNewAnswer}
+                style={{ width: "100%" }}
+              >
+                Dodaj odpowiedź
+              </Button>
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Button
+              variant="contained"
+              endIcon={<SendIcon />}
+              type="submit"
+              style={{ width: "100%" }}
+            >
+              Zapisz
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <Button
-            variant="contained"
-            endIcon={<SendIcon />}
-            type="submit"
-            style={{ width: "100%" }}
-          >
-            Zapisz
-          </Button>
-        </Grid>
-      </Grid>
+      )}
     </Box>
   );
 };
