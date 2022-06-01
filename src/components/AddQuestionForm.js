36829fd2b0947d430eed106c4a081d03ba7e5a83
Filip Wiśniewski <fi.wisniew@gmail.com>
2022-06-01 16:59:13 +0200

    PZSP2-94 Feat: Display number of questions

--- src/components/AddQuestionForm.js
+++ src/components/AddQuestionForm.js
@@ -2,29 +2,39 @@ import React from "react";
 import AddQuestion from "./AddQuestion";
 import { useState } from "react";
 import Button from "@mui/material/Button";
+import {Grid} from "@mui/material";
 
-const AddQuestionForm = ({ addQuestion, fetchCourseCodes }) => {
+const AddQuestionForm = ({ addQuestion, fetchCourseCodes, noQuestions }) => {
   const [showAddQuestion, setShowAddQuestion] = useState(false);
 
   return (
-    <div>
-      <div className="header">
-        <h2>Pytania</h2>
-        <Button
-          variant="contained"
-          color="success"
-          onClick={() => setShowAddQuestion(!showAddQuestion)}
-        >
-          Dodaj pytanie
-        </Button>
-      </div>
+    <Grid container>
+      <Grid item container className="header" alignItems='center' justifyContent='space-evenly' maxWidth>
+        <Grid item container spacing={0} xs={7} alignItems='center' justifyContent='center' direction='column'>
+          <Grid item>
+            <h2>Pytania</h2>
+          </Grid>
+          <Grid item>
+            <p>({noQuestions})</p>
+          </Grid>
+        </Grid>
+        <Grid item xs={5}>
+          <Button
+            variant="contained"
+            color="success"
+            onClick={() => setShowAddQuestion(!showAddQuestion)}
+          >
+            Dodaj pytanie
+          </Button>
+        </Grid>
+      </Grid>
       {showAddQuestion && (
         <AddQuestion
           addQuestion={addQuestion}
           fetchCourseCodes={fetchCourseCodes}
         />
       )}
-    </div>
+    </Grid>
   );
 };
 
