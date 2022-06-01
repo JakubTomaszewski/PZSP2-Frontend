901e236173c51050fa65b1cfd3097faa820c59f4
Jan Jędrzejewski <01158710@pw.edu.pl>
2022-06-01 20:49:17 +0200

    add new window for tests, load tests from database

--- src/components/AnsweredQuestion.js
+++ src/components/AnsweredQuestion.js
@@ -4,18 +4,10 @@ import Grid from "@mui/material/Grid";
 const AnsweredQuestion = (props) => {
     const answeredQ = props.ansQ
 
-    function showAnswers() {
-        if (answeredQ.questionAnswers.lenght === 0) return;
-        return(
-            answeredQ.questionAnswers.content.toString()
-        );
-    };
-
     return (
         <div className="answeredQuestion">
             <Grid className="answered-grid">
                 <p>{answeredQ.questionContent}</p>
-                {/* <p>{showAnswers()}</p> */}
                 <p>{answeredQ.studentAnswerContent}</p>
                 <form>
                     <label for="fname">Liczba punktów:</label>
--- src/components/BrowseTests.js
+++ src/components/BrowseTests.js
@@ -2,12 +2,8 @@ import React from "react";
 import { useState } from "react";
 import Test from "./Test"
 
-const BrowseTests = () => {
+const BrowseTests = ({tests, getSolutions}) => {
 
-    const [tests, setTests] = useState([
-        {testId: 1, name: "PIPR kolokwium 1", end_date: "24 maja 2022, 9:45", start_date: "24 maja 2022, 8:15", password: "sdw5sd"},
-        {testId: 2, name: "PIPR kolokwium 2", end_date: "6 czarwca 2022, 9:45", start_date: "6 czarwca 2022, 8:30", password: "pjt3g4"}
-    ])
     return (
         <div className="browseTests">
             <h1>Utworzone testy</h1>
