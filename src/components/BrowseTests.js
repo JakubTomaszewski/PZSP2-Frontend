b5655bb12acc32104770e439c14418738457bb6d
Jan Jędrzejewski <01158710@pw.edu.pl>
2022-06-01 21:16:56 +0200

    load solutions from database

--- src/components/BrowseTests.js
+++ src/components/BrowseTests.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 import Test from "./Test"
 
-const BrowseTests = ({tests, getSolutions}) => {
+const BrowseTests = ({tests}) => {
 
     return (
         <div className="browseTests">
